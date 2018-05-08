87510ccce77d6a11dfccddf09bd8d3cf21cb8ffc
Frexas <ruslanas.kudriavcevas@gmail.com>
2018-05-08 09:24:42 +0300

    Added iOS project for building possibility

---
+++ Examples/iOS/TestGPIO/TestGPIO/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestGPIO
+//
+//  Created by Ruslanas Kudriavcevas on 08/05/2018.
+//  Copyright © 2018 Mobon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
